76f0d77c20d43e95a77add739f063ed0b31059ea
valedamasco <valedamasco11@gmail.com>
2021-10-23 16:32:22 -0300

    Move from main scene to game one

--- TrickOrTreat/Assets/Scripts/HeartSystem.cs
+++ TrickOrTreat/Assets/Scripts/HeartSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeartSystem : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class HeartSystem : MonoBehaviour
         if (dead)
         {
             // Game over
+            SceneManager.LoadScene("PlayerScene");
         }
     }
 
