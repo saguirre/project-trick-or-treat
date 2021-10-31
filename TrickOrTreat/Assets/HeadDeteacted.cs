093144e297c5bf99ae736a0b6a867dca86ed1044
Guillermo de Armas <guillermo@zeppelinlabs.io>
2021-10-31 17:38:28 -0300

    agrego muerte de zombie

--- TrickOrTreat/Assets/HeadDeteacted.cs
+++ TrickOrTreat/Assets/HeadDeteacted.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class HeadDeteacted : MonoBehaviour
 {
+    private Vector3 movement;
     GameObject Enemy;
+    bool dead = false;
     // Start is called before the first frame update
     void Start()
     {
         Enemy = gameObject.transform.parent.gameObject;
     }
 
+    void Update()
+    {
+        if(dead){
+        movement = new Vector3(0f, -2,0f);
+        Enemy.transform.position = Enemy.transform.position + movement * Time.deltaTime;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GetComponent<Collider2D>().enable = false;
-        Enemy.GetComopnent<SpriteRender>().flipY = true;
-        Enemy.GetComopnent<Collider2D>().enable = false;
-        Vector3 movemoent = new Vector3(Random.Range(40,70), Random.Range(-40,40), 0f);
-        Enemy.transform.position += movemoent * Time.deltaTime;
+        GetComponent<Collider2D>().enabled = false;
+        Enemy.GetComponent<SpriteRenderer>().flipY = true;
+        Enemy.GetComponent<Collider2D>().enabled = false;
+        dead = true;
     }
 }
--- TrickOrTreat/Assets/ZombiesFromFloor.cs
+++ TrickOrTreat/Assets/ZombiesFromFloor.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ZombiesFromFloor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
