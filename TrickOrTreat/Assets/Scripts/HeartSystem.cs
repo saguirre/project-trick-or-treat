7aa478e534d9beed6ffbc885a940f5726d6fe64e
Gonzalo Lamela <gonlam67@gmail.com>
2021-11-29 23:49:57 -0300

    Sprite player damage

--- TrickOrTreat/Assets/Scripts/HeartSystem.cs
+++ TrickOrTreat/Assets/Scripts/HeartSystem.cs
@@ -24,7 +24,6 @@ public class HeartSystem : MonoBehaviour
         if (dead)
         {
             SceneManager.LoadScene("GameOver");
-            //SceneManager.LoadScene("PlayerScene");
         }
     }
 
