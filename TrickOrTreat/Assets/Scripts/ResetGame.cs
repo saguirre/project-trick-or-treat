13fc026424d00a56c9ffb3fc699e391d9e00351a
Gonzalo Lamela <gonlam67@gmail.com>
2021-12-07 00:48:12 -0300

    Agrego un reset de emergencia para no cerrar y abrir el juego (apretar 1, 2, 3 y 0)

---
+++ TrickOrTreat/Assets/Scripts/ResetGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.Alpha0) && Input.GetKey(KeyCode.Alpha2) && Input.GetKey(KeyCode.Alpha3) && Input.GetKey(KeyCode.Alpha0))
+        {
+            Debug.Log("Reset!");
+            Singleton.Instance.inv = new List<Key.KeyType>();
+            SceneManager.LoadScene("Intro");
+        }
+    }
+}
