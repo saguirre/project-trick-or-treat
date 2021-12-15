cf2f4bb256de65c8ea8c23a789240b3d4422954a
Gonzalo Lamela <gonlam67@gmail.com>
2021-12-14 23:06:45 -0300

    Se borran Componentes Rigidbody2d en calaveras y se soluciona el tema del desvio. se unifica código en un solo script

--- TrickOrTreat/Assets/Scripts/MovingSkulls.cs
+++ TrickOrTreat/Assets/Scripts/MovingSkulls.cs
@@ -23,4 +23,16 @@ public class MovingSkulls : MonoBehaviour
         transform.position += movement * Time.fixedDeltaTime * moveSpeed;
         transform.rotation = Quaternion.identity;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("SkullMovementEndPosition"))
+        {
+            transform.SetPositionAndRotation(new Vector2(2f, Random.Range(-0.175f, 0.378f)), Quaternion.identity);
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            Physics2D.IgnoreCollision(collision.otherCollider, collision.collider);
+        }
+    }
 }
