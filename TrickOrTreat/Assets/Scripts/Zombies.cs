c084e779792e965859b77fd48ed39eb00599b993
Santi Aguirre <saguirrews@gmail.com>
2021-12-15 22:38:28 -0300

    Finished zombie bugfixes.

--- TrickOrTreat/Assets/Scripts/Zombies.cs
+++ TrickOrTreat/Assets/Scripts/Zombies.cs
@@ -22,7 +22,11 @@ public class Zombies : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(1, 4));
             var positionIndex = Random.Range(-8, 8);
-            Instantiate(zombies, new Vector3(positionIndex * 0.2F, -0.07F, 0), Quaternion.identity);
+            var zombo = Instantiate(zombies, new Vector3(positionIndex * 0.2F, -0.07F, 0), Quaternion.identity);
+            zombo.GetComponent<BoxCollider2D>().enabled = false;
+            yield return new WaitForSeconds(0.5f);
+            zombo.GetComponent<BoxCollider2D>().enabled = true;
+
         }
     }
 }
